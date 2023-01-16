640fd5a56f4c2a61da51a3c45af38fba6624c6c6
fullzer4 <gabrielpelizzaro@gmail.com>
2023-01-16 00:33:23 -0300

    tests

---
+++ src/frontend/src/Predict.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { QueryClient, QueryClientProvider } from "react-query"
+
+function App() {
+  
+
+
+  return (
+    <div className="Predict">
+      
+    </div>
+  )
+}
+
+export default App
--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import App from './Predict'
+import { QueryClient } from "react-query"
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
