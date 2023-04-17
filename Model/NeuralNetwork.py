e8fd9dda2367f24bd8c6929ff19d78bab7b5db6d
fullzer4 <gabrielpelizzaro@gmail.com>
2023-04-17 03:39:21 -0300

    new

--- Model/NeuralNetwork.py
+++ Model/NeuralNetwork.py
@@ -7,33 +7,38 @@ from sklearn.metrics import r2_score, mean_squared_error
 from torch.utils.data import TensorDataset, DataLoader
 import json
 
-# rodar gpu
-#device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# ativar modo gpu comentar linha 12 e descomentar a abaixo
+# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("cpu")
 print(f'Using: {device}')
 
+# preparando dados e separando valores para o modelo
 df = pd.read_csv("./data/data.csv") 
 x = df[['m (kg)','Mt','ec (cm3)','ep (KW)','Fuel consumption']].values
 y = df[["Ewltp (g/km)", 'Enedc (g/km)']].values
 
+# separando os dados em ocnjuntos de teste e treino
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
+# tirando a media, o desvio padrao e normalizando os valores
 x_train_mean = x_train.mean(axis=0)
 x_train_std = x_train.std(axis=0)
 x_train = (x_train - x_train_mean) / x_train_std
 x_test = (x_test - x_train_mean) / x_train_std
 
+# alocando os nossos dados em tensores
 x_train = torch.from_numpy(x_train).float().to(device)
 y_train = torch.from_numpy(y_train).float().to(device)
 x_test = torch.from_numpy(x_test).float().to(device)
 y_test = torch.from_numpy(y_test).float().to(device)
 
-
+# salvar dados no json da media e do desvio padrao para usar futuramente
 vars = {"x_train_mean": x_train_mean.tolist(), "x_train_std": x_train_std.tolist()}
 
 with open("../backend/values.json", "w") as f:
     json.dump(vars, f)
 
+# classe da rede neural
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim1, hidden_dim2, output_dim):
         super(NeuralNetwork, self).__init__()
@@ -51,12 +56,14 @@ class NeuralNetwork(nn.Module):
         x = self.fc3(x)
         return x
 
+# definindo super parametros
 input_dim = 5
 hidden_dim1 = 64
 hidden_dim2 = 64
 output_dim = 2
 model = NeuralNetwork(input_dim, hidden_dim1, hidden_dim2, output_dim).to(device)
 
+# definindo parametros para avaliacao do modelo
 criterion = nn.MSELoss()
 reg = nn.L1Loss()
 
@@ -66,6 +73,7 @@ def regularization(model, reg_lambda):
         reg_loss += reg(param, torch.zeros_like(param))
     return reg_loss * reg_lambda
 
+# taxa de aprendizado
 reg_lambda = 0.001
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
 
@@ -77,13 +85,14 @@ batch_size = 64 # 32
 train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
 
+#numero de epochs ou seja vezes que vai treinar a rede
 num_epochs = 15000
 for epoch in range(num_epochs):
     for i, (x_batch, y_batch) in enumerate(train_loader):
 
         outputs = model(x_batch)
         loss = criterion(outputs, y_batch)
-        reg_loss = regularization(model, reg_lambda)
+        reg_loss = regulariz=ation(model, reg_lambda)
         loss += reg_loss
 
         optimizer.zero_grad()
--- backend/index.py
+++ backend/index.py
@@ -5,9 +5,11 @@ from flask_cors import CORS
 import pandas as pd
 import json
 
+# criando servidor flask
 app = Flask(__name__)
 CORS(app)
 
+# modelo da rede neural
 class NeuralNetwork(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(NeuralNetwork, self).__init__()
@@ -21,11 +23,13 @@ class NeuralNetwork(nn.Module):
         x = self.fc2(x)
         return x
 
+# abrindo os valores salvos anteriomente e retratando eles
 with open('values.json', 'r') as f:
     data = json.load(f)
 x_train_mean = torch.tensor(data['x_train_mean'], dtype=torch.float)
 x_train_std = torch.tensor(data['x_train_std'], dtype=torch.float)
 
+#super parametros
 input_dim = 5
 hidden_dim = 64
 output_dim = 2
@@ -33,6 +37,7 @@ model = NeuralNetwork(input_dim, hidden_dim, output_dim)
 model.load_state_dict(torch.load('model.pt'))
 model.eval()
 
+# rota para prever
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
@@ -42,5 +47,6 @@ def predict():
     output = {'prediction': prediction.tolist()}
     return jsonify(output)
 
+# abrir o app para o host
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
